e3ec7903a1ca18c92c0c7d973c8b821d1eabbfb5
Valentin <45029982+ValentinMorin@users.noreply.github.com>
2020-03-10 10:34:51 +0100

    Fenêtre 1.0

---
+++ UI/app.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# Création de la fenêtre
+window = Tk()
+window.title("e-Crypt")
+window.geometry("480x360")
+window.minsize(480, 360)
+window.iconbitmap("icon.ico")
+window.config(background='#656767')
+
+# Ajouter un premier texte
+
+# Afficher
+window.mainloop()
