1d6f62b91a6c0bdd1f55a4d234963f8539605bdc
Valentin <45029982+ValentinMorin@users.noreply.github.com>
2020-03-28 14:39:07 +0100

    Ajout menus

--- security/app.py
+++ security/app.py
@@ -3,18 +3,20 @@ from tkinter.filedialog import askopenfilename
 import hashlib
 import Crypto.Cipher
 
-
-
+# Enregistre le choix de la méthode de Hash
 def select(event):
     selection_hash = choice.selection_get()
     affichage_hash.configure(text=selection_hash)
 
+# Action pour sélectionner le fichier
 def buttonFonction():
     filename.set(askopenfilename(filetypes=FILETYPES))
 
+# Selectionner le choix du Hash
 def hash(selection_hash):
     hash = selection_hash
 
+# Méthode de Hash
 def hashfile():
     if affichage_hash['text'] == 'SHA-1':
         with open(filename.get(),'rb') as file:
@@ -47,33 +49,55 @@ def hashfile():
         file.close()
         print(hash)
 
+# Action du menu Générer des clés
+def show_key():
+    about_key = Toplevel(mb)
+    about_key.title("Générer des clés")
+    about_key.geometry("600x430")
+    about_key.minsize(600, 430)
+
+# Action du menu Chiffrer un fichier
+def show_chiff():
+    about_chiff = Toplevel(mb)
+    about_chiff.title("Chiffrer un fichier")
+    about_chiff.geometry("600x430")
+    about_chiff.minsize(600, 430)
+
+# Type de fichiers
 FILETYPES = [("text files", "*.txt")]
 
 # Création de la fenêtre
 window = Tk()
 window.title("e-Crypt")
-window.geometry("700x530")
-window.minsize(700, 530)
-window.config(background='#d6d6d6')
+window.geometry("600x430")
+window.minsize(600, 430)
 
-# Menu fenêtre
-menu = Frame(window, borderwidth=3, bg='#d6d6d6')
-menu.pack()
+# Widgets
+mb = Menubutton(window,text="Menu")
+mb.menu = Menu(mb, tearoff=0)
+mb["menu"] = mb.menu
+mb.config(background='#D6D6D6', fg='#000000')
 
-# Menu Clés
-cle = Menubutton(menu, text='Vos clés', width='20', borderwidth=2, activebackground='gray',relief = RAISED)
-cle.pack()
+mb.menu.add_command(label="Générer des clés", command=show_key)
+mb.menu.add_command(label="Chiffrer un fichier", command=show_chiff)
+mb.menu.add_separator()
+mb.menu.add_command(label="Quitter", command=mb.quit)
+mb.pack(side=TOP)
 
-# frame selection fichiers
+# Titre Fenêtre
+titre_type= Label(window, text="Hasher un fichier", font=("Arial", 18), fg='#000000')
+titre_type.pack(pady= 20)
+
+# Frame selection fichiers
 frame_selection_fichier = Frame(window, relief='solid')
-frame_selection_fichier.pack(side=RIGHT)
+frame_selection_fichier.pack(side=RIGHT, padx=110)
 
 # Frame algo
-frame_algo = Frame(window, bg="#656767", bd=2, relief='solid')
+frame_algo = Frame(window, bg="#D6D6D6", bd=2, relief='solid')
 frame_algo.pack(side=LEFT)
 
 # Ajouter un premier texte
-label_type = Label(frame_algo, text="Algorithm", font=("Arial", 10), bg='#656767', fg='#ffffff')
+label_type = Label(frame_algo, text="Methode", font=("Arial", 10), bg='#D6D6D6', fg='#000000')
 label_type.pack()
 
 # Choix du Hash
@@ -92,18 +116,17 @@ affichage_hash = Label(frame_algo, text="")
 affichage_hash.pack()
 
 # bouton selection de fichier et affichage du chemin d'accès
-bouton_fichier = Button(frame_selection_fichier,text="Select Files", command=buttonFonction)
+bouton_fichier = Button(frame_selection_fichier,text="Sélectionnez un fichier", command=buttonFonction)
 bouton_fichier.pack()
 
 filename = StringVar(window)
 
-entry = Entry(frame_selection_fichier, textvariable=filename, width=70)
+entry = Entry(frame_selection_fichier, textvariable=filename, width=50)
 entry.pack()
 
 # bouton de cryptage
-bouton_cryptage = Button(frame_selection_fichier, text="HASH", command=hashfile)
+bouton_cryptage = Button(frame_selection_fichier, text="Hasher", command=hashfile)
 bouton_cryptage.pack()
-
 
 # Afficher
 window.mainloop()
