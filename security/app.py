b639e5d989c68994dad593cbe66716bfb76464dc
Valentin <45029982+ValentinMorin@users.noreply.github.com>
2020-03-31 00:18:55 +0200

    Interface chiffrement / Chiffrement / Suppresion clé

--- security/app.py
+++ security/app.py
@@ -2,32 +2,22 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter import ttk
 import hashlib
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-import Crypto.Random as RNG
-from Crypto import Random
-
+from cryptography.fernet import Fernet, MultiFernet
 import json
 
 # Enregistre le choix de la méthode de Hash
-
-
 def select(event):
     selection_hash = choice.selection_get()
     affichage_hash.configure(text=selection_hash)
 
 # Enregistre le choix du sel ou non
-
-
 def sel():
     selection = str(var.get())
     affichage_sel.config(text=selection)
 
-
 # Action pour sélectionner le fichier
 def buttonFonction():
     filename.set(askopenfilename(filetypes=FILETYPES))
-
 
 # Méthode de Hash
 def hashfile():
@@ -116,13 +106,11 @@ def hashfile():
             mas.set(hash)
 
 # Action du menu Générer des clés
-
-
 def show_key():
     about_key = Toplevel(mb)
     about_key.title("Générer des clés")
-    about_key.geometry("800x430")
-    about_key.minsize(800, 430)
+    about_key.geometry("1200x400")
+    about_key.minsize(1200, 400)
 
     # Enregistre le choix de la taille de la clé AES
     def select_key(event):
@@ -133,39 +121,56 @@ def show_key():
     def genera_key():
         if result_key['text'] == '128 bits':
             with open('table_key.json', 'a') as file:
-                key = get_random_bytes(16)
-                nbr_key = str(key)
+                key = Fernet.generate_key()
+                nbr_key = key.decode()
                 global pseudo
                 global password
                 pseudo = pseudo_entry.get()
                 password = password_entry.get()
-                x = {'Pseudo': pseudo, 'Password': password,
-                     'Key 128 bits': nbr_key}
+                x = {'Pseudo': pseudo, 'Password': password,'Key': nbr_key}
                 y = json.dumps(x, indent=1)
                 file.write(y)
+                table.insert('',END,values=(pseudo, nbr_key))
                 file.close()
         if result_key['text'] == '192 bits':
             with open('table_key.json', 'a') as file:
-                key = get_random_bytes(24)
-                nbr_key = str(key)
-                x = {'Pseudo': pseudo, 'Password': password,
-                     'Key 192 bits': nbr_key}
+                key = Fernet.generate_key()
+                nbr_key = key.decode()
+                pseudo = pseudo_entry.get()
+                password = password_entry.get()
+                x = {'Pseudo': pseudo, 'Password': password,'Key': nbr_key}
                 y = json.dumps(x, indent=1)
                 file.write(y)
+                table.insert('',END,values=(pseudo, nbr_key))
                 file.close()
         if result_key['text'] == '256 bits':
             with open('table_key.json', 'a') as file:
-                key = get_random_bytes(32)
-                nbr_key = str(key)
-                x = {'Pseudo': pseudo, 'Password': password,
-                     'Key 256 bits': nbr_key}
+                key = Fernet.generate_key()
+                nbr_key = key.decode()
+                pseudo = pseudo_entry.get()
+                password = password_entry.get()
+                x = {'Pseudo': pseudo, 'Password': password,'Key': nbr_key}
                 y = json.dumps(x, indent=1)
                 file.write(y)
+                table.insert('',END,values=(pseudo, nbr_key))
                 file.close()
 
+    # Action supprimer une clé
+    def delete_key():
+        select_table = table.selection()
+        for selected_item in select_table:
+            table.delete(selected_item)
+            with open('table_key.json', 'w') as file:
+                data_delete = json.load(file)
+                board = (data['Pseudo'], data['Key'])
+                boards = (data['Pseudo'], data['Password'], data['Key'])
+                if selected_item == board:
+                    file.pop(boards)
+                file.close()
+        
+
     # Titre
-    titre_key = Label(about_key, text="Générer des clés",
-                      font=("Arial", 18), fg='#000000')
+    titre_key = Label(about_key, text="Générer des clés",font=("Arial", 18), fg='#000000')
     titre_key.pack()
 
     # Frame clés
@@ -173,8 +178,7 @@ def show_key():
     frame_key.pack(side=LEFT)
 
     # Ajouter un premier texte
-    text_key = Label(frame_key, text="Taille des clés AES",
-                     font=("Arial", 10), bg='#D6D6D6', fg='#000000')
+    text_key = Label(frame_key, text="Taille des clés AES",font=("Arial", 10), bg='#D6D6D6', fg='#000000')
     text_key.pack()
 
     # Choix de la taille
@@ -182,7 +186,6 @@ def show_key():
     choice_key.insert(1, "128 bits")
     choice_key.insert(2, "192 bits")
     choice_key.insert(3, "256 bits")
-
     choice_key.bind('<<ListboxSelect>>', select_key)
     choice_key.pack()
     result_key = Label(frame_key, text="")
@@ -203,27 +206,105 @@ def show_key():
     password_entry.pack()
 
     # Bouton générer la clé
-    bouton_key = Button(about_key, text="Générer", width=10,
-                        height=3, font=("Arial", 10), command=genera_key)
+    bouton_key = Button(about_key, text="Générer", width=10,height=3, font=("Arial", 10), command=genera_key)
     bouton_key.pack(pady=150, side=LEFT)
 
     # Tableau
     frame_table = Frame(about_key, relief='solid')
     frame_table.pack()
+    table=ttk.Treeview(about_key, show="headings")
+    table["columns"]=(1, 2)
+    table.column("#0", width=0)
+    table.column(1, width=150, minwidth=150)
+    table.column(2, width=350, minwidth=350)
+    table.heading(1, text="Pseudo",anchor=W)
+    table.heading(2, text="Key",anchor=W)
+    selected_item = table.selection()
+    with open('table_key.json') as f:
+        data = json.load(f)
+        table.insert('',END,values=(data['Pseudo'],data['Key']))
+        table.pack()
+        f.close()
+
+    # Bouton Supprimer
+    supprime_key = Button(about_key, text="Supprimer", width=10,height=3, font=("Arial", 10), command=delete_key)
+    supprime_key.pack(side=TOP)
 
 # Action du menu Chiffrer un fichier
-
-
 def show_chiff():
     about_chiff = Toplevel(mb)
     about_chiff.title("Chiffrer un fichier")
-    about_chiff.geometry("600x430")
-    about_chiff.minsize(600, 430)
+    about_chiff.geometry("600x600")
+    about_chiff.minsize(600, 600)
+
+    def buttonFonction2():
+        filename2.set(askopenfilename(filetypes=FILETYPES))
 
     # Titre
-    titre_type = Label(about_chiff, text="Chiffrer un fichier",
-                       font=("Arial", 18), fg='#000000')
+    titre_type = Label(about_chiff, text="Chiffrer / Déchiffrer un fichier", font=("Arial", 18), fg='#000000')
     titre_type.pack()
+
+    # Tableau
+    frame_table = Frame(about_chiff, relief='solid')
+    frame_table.pack()
+    demande_type = Label(about_chiff, text="Sélectionnez votre clé", font=("Arial", 10), fg='#000000')
+    demande_type.pack(pady=20)
+    table=ttk.Treeview(about_chiff, show="headings")
+    table["columns"]=(1, 2)
+    table.column("#0", width=0)
+    table.column(1, width=150, minwidth=150)
+    table.column(2, width=350, minwidth=350)
+    table.heading(1, text="Pseudo",anchor=W)
+    table.heading(2, text="Key",anchor=W)
+    selected_item = table.selection()
+    with open('table_key.json') as f:
+        data = json.load(f)
+        table.insert('',END,values=(data['Pseudo'],data['Key']))
+        table.pack()
+    f.close()
+
+    # Bouton chiffrement
+    def btn_chiffr():
+        with open('table_key.json', 'r') as f:
+            data = json.load(f)
+            verif = (data['Password'])
+            entry_verif = password_entry.get()
+            if entry_verif == verif:
+                select_table = table.selection()
+                for selected_item in select_table:
+                    select_key = selected_item('Key')
+                    final_key = select_key.encode()
+                    with open(filename2.get(), 'rb') as file:
+                        token = final_key.encrypt(file)
+                    file.close()
+
+    # Frame Select fichier chiffrement
+    frame_select_fic = Frame(about_chiff, relief='solid')
+    frame_select_fic.pack(side=BOTTOM, ipady=20)
+    bouton_fichier = Button(frame_select_fic, text="Sélectionnez un fichier", command=buttonFonction2)
+    bouton_fichier.pack()
+
+    filename2 = StringVar(window)
+
+    entry2 = Entry(frame_select_fic, textvariable=filename2, width=100)
+    entry2.pack()
+
+    # Frame + Bouton chiffre / déchiffre
+    frame_chiff = Frame(about_chiff, relief='solid')
+    frame_chiff.pack()
+    button_chiffrer = Button(frame_chiff, text="Chiffrer", width=10,height=2, font=("Arial", 10), command=btn_chiffr)
+    button_chiffrer.pack(side=LEFT)
+
+    button_dechiffrer = Button(frame_chiff, text="Déchiffrer", width=10,height=2, font=("Arial", 10))
+    button_dechiffrer.pack(side=RIGHT)
+
+    # Mot de passe
+    frame_pass = Frame(about_chiff, relief='solid')
+    frame_pass.pack(pady=50)
+    password_text = Label(frame_pass, text="Mot de passe")
+    password_text.pack()
+    password_entry = Entry(frame_pass, textvariable="", width=20)
+    password_entry.pack()
 
 
 # Type de fichiers
@@ -248,8 +329,7 @@ mb.menu.add_command(label="Quitter", command=mb.quit)
 mb.pack(side=TOP)
 
 # Titre Fenêtre
-titre_type = Label(window, text="Hasher un fichier",
-                   font=("Arial", 18), fg='#000000')
+titre_type = Label(window, text="Hasher un fichier", font=("Arial", 18), fg='#000000')
 titre_type.pack(pady=20)
 
 # Frame selection fichiers
@@ -261,8 +341,7 @@ frame_algo = Frame(window, bg="#D6D6D6", bd=2, relief='solid')
 frame_algo.pack(side=LEFT)
 
 # Ajouter un premier texte
-label_type = Label(frame_algo, text="Methode", font=(
-    "Arial", 10), bg='#D6D6D6', fg='#000000')
+label_type = Label(frame_algo, text="Methode", font=("Arial", 10), bg='#D6D6D6', fg='#000000')
 label_type.pack()
 
 # Choix du Hash
@@ -281,8 +360,7 @@ affichage_hash = Label(frame_algo, text="")
 affichage_hash.pack()
 
 # Bouton selection de fichier et affichage du chemin d'accès
-bouton_fichier = Button(frame_selection_fichier,
-                        text="Sélectionnez un fichier", command=buttonFonction)
+bouton_fichier = Button(frame_selection_fichier, text="Sélectionnez un fichier", command=buttonFonction)
 bouton_fichier.pack()
 
 filename = StringVar(window)
@@ -290,32 +368,26 @@ filename = StringVar(window)
 entry = Entry(frame_selection_fichier, textvariable=filename, width=100)
 entry.pack()
 
-
 # Insérer le sel
 label_sel = Label(frame_selection_fichier, text="Entrez votre sel (optionnel)")
 entry_sel = Entry(frame_selection_fichier)
 label_sel.pack()
 entry_sel.pack()
 
-
 # Bouton de cryptage
-bouton_cryptage = Button(frame_selection_fichier,
-                         text="Hasher", command=hashfile)
+bouton_cryptage = Button(frame_selection_fichier, text="Hasher", command=hashfile)
 bouton_cryptage.pack()
 
 # Instancier le résultat du Hash
 mas = StringVar()
-text_hash = Entry(frame_selection_fichier, textvariable=mas, width=150,
-                  font=("Arial", 8), fg='#000000')
+text_hash = Entry(frame_selection_fichier, textvariable=mas, width=150, font=("Arial", 8), fg='#000000')
 text_hash.pack()
 
 # Radio button sel
 var = StringVar()
-R1 = Radiobutton(frame_algo, text="Avec sel", variable=var, value="Avec sel",
-                 command=sel,  bg='#D6D6D6')
+R1 = Radiobutton(frame_algo, text="Avec sel", variable=var, value="Avec sel", command=sel,  bg='#D6D6D6')
 R1.pack()
-R2 = Radiobutton(frame_algo, text="Sans sel", variable=var, value="Sans sel",
-                 command=sel,  bg='#D6D6D6')
+R2 = Radiobutton(frame_algo, text="Sans sel", variable=var, value="Sans sel", command=sel,  bg='#D6D6D6')
 R2.pack()
 
 affichage_sel = Label(frame_algo, text="")
@@ -323,8 +395,7 @@ affichage_sel.pack()
 
 frame_nom = Frame(frame_selection_fichier)
 frame_nom.pack()
-Name_label = Label(
-    frame_nom, text="Valentin Morin & Maxime Préard", font=("Arial", 7))
+Name_label = Label(frame_nom, text="Valentin Morin & Maxime Préard", font=("Arial", 7))
 Name_label.pack()
 
 # Afficher
